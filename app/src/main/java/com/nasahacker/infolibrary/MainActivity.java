b73602276bbbf3ea343b041e29ec6a92dc0625ed
Tamim Hossain <tamimh.dev@gmail.com>
2024-07-17 20:56:18 +0600

    Info

---
+++ app/src/main/java/com/nasahacker/infolibrary/MainActivity.java
@@ -0,0 +1,42 @@
+package com.nasahacker.infolibrary;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.nasahacker.info.NasaInfo;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) ->
+        {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        findViewById(R.id.txtHello).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                NasaInfo.setListener(MainActivity.this,getString(R.string.app_name));
+            }
+        });
+
+
+    }
+}
